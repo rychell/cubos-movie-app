0f2cdaadf045bb267a2a394355bf2a119f07fa1d
Rychell Sousa <rychell@gmail.com>
2021-08-03 17:25:04 -0300

    test(MovieCard): render test

--- src/components/MovieCard/__tests__/movieCard.spec.tsx
+++ src/components/MovieCard/__tests__/movieCard.spec.tsx
@@ -4,20 +4,33 @@ import { MovieCard } from '..';
 
 describe("Movie Card component", () => {
     it("Renders correctly", () => {
-        const { getByTestId } = render(<MovieCard />)
+        const movie = {
+            cover: 'fake-cover',
+            title: 'fake-title',
+            releasedAt: 'fake date',
+            rating: 5,
+            description: 'fake description',
+            genres: [
+                { name: "fake genre 1" },
+                { name: "fake genre 2" },
+                { name: "fake genre 3" }
+            ]
+        }
+
+        const { getByTestId } = render(<MovieCard {...movie} />)
 
         const imageElement = getByTestId('cover-image')
         const titleElement = getByTestId('title')
-        const descriptionElement = getByTestId('description')
+        const releasedDateElement = getByTestId('released-date')
         const ratingElement = getByTestId('rating')
-        const releasedDateElement = getByTestId('release-date')
+        const descriptionElement = getByTestId('description')
         const genresElement = getByTestId('genres')
 
-        expect(imageElement).toBeInTheDocument()
-        expect(titleElement).toBeInTheDocument()
-        expect(descriptionElement).toBeInTheDocument()
-        expect(ratingElement).toBeInTheDocument()
-        expect(releasedDateElement).toBeInTheDocument()
-        expect(genresElement).toBeInTheDocument()
+        expect(imageElement.getAttribute("src")).toBe(movie.cover)
+        expect(titleElement.textContent).toBe(movie.title)
+        expect(descriptionElement.textContent).toBe(movie.description)
+        expect(ratingElement.textContent).toBe(`${movie.rating}`)
+        expect(releasedDateElement.textContent).toBe(movie.releasedAt)
+        expect(genresElement.childElementCount).toBe(movie.genres.length)
     })
 })
