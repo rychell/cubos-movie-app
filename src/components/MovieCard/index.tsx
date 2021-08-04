2f9cb29b71b54c2dbd558788e06aae73d30a657f
Rychell Sousa <rychell@gmail.com>
2021-08-04 19:12:14 -0300

    layout: general ajusments

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,36 +1,41 @@
+import { Link } from 'react-router-dom'
 import { IMovie } from '../../@types/IMovie'
 import { Chip } from '../Chip'
 
 import styles from './styles.module.css'
 
 const MovieCard = (props: IMovie) => {
-    const { cover,
+    const { 
+        id,
+        cover,
         title,
         releasedAt,
         rating,
         description,
         genres } = props
     return (
-        <article className={styles.wrapper}>
-            <div className={styles.coverImage}>
-                <img src={cover} alt="" data-testid="cover-image" />
-            </div>
-            <div className={styles.movieDetails}>
-                <header>
-                    <h2 data-testid="title">{title}</h2>
-                    <span className={styles.releasedDate} data-testid="released-date">{releasedAt.toLocaleDateString()}</span>
-                    <span className={styles.rating} data-testid="rating">{rating * 10}%</span>
-                </header>
-                <main>
-                    <p data-testid="description">{description}</p>
-                    <div className={styles.tags} data-testid="genres">
-                        {genres.map((genre, index) => {
-                            return <Chip key={index} text={genre.name} />
-                        })}
-                    </div>
-                </main>
-            </div>
-        </article>
+        <Link to={`/movie/${id}`}>
+            <article className={styles.wrapper}>
+                <div className={styles.coverImage}>
+                    <img src={cover} alt="" data-testid="cover-image" />
+                </div>
+                <div className={styles.movieDetails}>
+                    <header>
+                        <h2 data-testid="title">{title}</h2>
+                        <span className={styles.releasedDate} data-testid="released-date">{releasedAt.toLocaleDateString()}</span>
+                        <span className={styles.rating} data-testid="rating">{rating * 10}%</span>
+                    </header>
+                    <main>
+                        <p data-testid="description">{description}</p>
+                        <div className={styles.tags} data-testid="genres">
+                            {genres.map((genre, index) => {
+                                return <Chip key={index} text={genre.name} />
+                            })}
+                        </div>
+                    </main>
+                </div>
+            </article>
+        </Link>
     )
 }
 
